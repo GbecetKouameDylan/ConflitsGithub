4ec3bf1c486b6c71dc1e7ebb9ff56b777854d624
D <2164425@cegepmontpetit.ca>
2024-03-18 10:43:02 -0400

    .

--- app1 - Copie (2)/src/app/app.component.ts
+++ app1 - Copie (2)/src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app1';
 
-
+  incroyable() {
+    return "Je n'en reviens pas!"
+  }
 }
--- app1 - Copie/src/app/app.component.ts
+++ app1 - Copie/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app1';
   wow() {
+    return "Ben voyons donc!"
   }
   incroyable() {
 
